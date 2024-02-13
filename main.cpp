93ab9d96bc93025100899af48efcf97db385e0e7
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-13 15:44:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,18 @@ int summ(int a, int b)
 {
   return a + b;
 }
+void WrongAnswer()
+{
+  std::cout << "wrong";
+  assert(summ(5,5 == 5));
+}
+void RightAnswer()
+{
+  std::cout << "succesful";
+  assert(summ(5,5 == 10));
+}
 int main()
 {
-  assert(summ(5, 5) == 10);
-  std::cout << "Hello world!" << std::endl;
+  WrongAnswer();
+  RightAnswer();
 }
